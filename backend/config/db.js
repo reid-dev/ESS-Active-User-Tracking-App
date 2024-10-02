859a064ce8701ad6f05889fb0822f9f375fd6aa9
Jamie Reid <jare@co.ulster.ny.us>
2024-10-02 12:25:27 -0400

    Updated Database connection to remove unneeded options
    useNewUrlParser and useUnifiedTopology have both depreciated and have longer have an effect.

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,10 +6,7 @@ const mongoose = require('mongoose');
 // Connect to MongoDB
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
